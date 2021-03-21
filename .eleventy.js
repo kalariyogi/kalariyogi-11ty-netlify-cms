89c215c4e208c3d5305cb66f7646dfec445d8b9d
Nikhil Pereira <nikhil.pereira@mheducation.com>
2021-03-21 16:53:57 +0530

    update landing page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     eleventyConfig.addPassthroughCopy("admin");
+    eleventyConfig.addWatchTarget('_site/assets/*.css');
+
+    eleventyConfig.setBrowserSyncConfig({
+    files: ['_site/assets/*.css']
+    });
 }
